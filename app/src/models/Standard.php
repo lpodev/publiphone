df360d550d9be5322f04223948ebda8a8097fff2
lpodev <lpodevco@gmail.com>
2022-12-19 14:42:37 +0100

    Complete Standard class

--- app/src/models/Standard.php
+++ app/src/models/Standard.php
@@ -3,6 +3,33 @@
 class Standard
 {
     //Private Attributes
-    private $line;
-    private $delay;
+    private int $delay;
+    private string $phoneNumber;
+    private float $price;
+    private float $balance;
+    private int $duration;
+
+    public function __construct(int $delay, string $phoneNumber, float $price, float $balance, int $duration)
+    {
+        $this->delay = $delay;
+        $this->phoneNumber = $phoneNumber;
+        $this->price = $price;
+        $this->balance = $balance;
+        $this->duration = $duration;
+    }
+
+    public function ConsumeBalance(): void
+    {
+        // to complete
+    }
+
+    public function startDelay(): void
+    {
+        // to complete
+    }
+
+    public function hangUp(): float
+    {
+        return $this->duration;
+    }
 }
